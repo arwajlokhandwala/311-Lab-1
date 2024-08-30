f6d2d5f3d669f48ba080f1e971a01bdd4774acd2
Arwa Lokhandwala <arwajlokhandwala@gmail.com>
2024-08-30 14:51:10 -0400

    Added comments for all classes

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,28 +1,32 @@
 package org.example;
 
 
-
 public class Car extends Vehicle {
 
-    private String brand;
+    private String brand; // brand of Car
 
+    // parameterized constructor for Car object
     public Car(int numberOfWheels, String color, float engineSize, String fuelType, String brand){
         super(numberOfWheels, color, engineSize, fuelType);
         this.brand = brand;
     }
 
+    // accessor method for brand name
     public String getBrand() {
         return brand;
     }
 
+    //mutator method for brand name
     public void setBrand(String brand) {
         this.brand = brand;
     }
 
+    // honk() method - prints "Honk,honk!"
     public void honk(){
         System.out.println("Honk, honk!");
     }
 
+    // displayInfo method - displays car info including brand, fuel type, engine size, number of wheels and color
     public String displayInfo(){
 
         return "Car Brand: " + brand +
--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -2,11 +2,12 @@ package org.example;
 
 public class Vehicle {
 
-   private int numberOfWheels ;
-   private String color;
-   private float engineSize;
-   private String fuelType;
+   private int numberOfWheels ; //  number of wheels on vehicle
+   private String color; // color of vehicle
+   private float engineSize; // engine size of vehicle
+   private String fuelType; // fuel type used by vehicle
 
+    // default constructor
    public Vehicle(){
        numberOfWheels = 0;
        color = "N/A";
@@ -14,6 +15,7 @@ public class Vehicle {
        fuelType = "N/A";
    }
 
+   // parameterized constructor
    public Vehicle(int numberOfWheels, String color, float engineSize, String fuelType) {
        this.numberOfWheels = numberOfWheels;
        this.color = color;
@@ -21,34 +23,42 @@ public class Vehicle {
        this.fuelType = fuelType;
    }
 
+   // accessor method for engineSize
     public float getEngineSize() {
         return engineSize;
     }
 
+    //accessor method for numberOfWheels
     public int getNumberOfWheels() {
         return numberOfWheels;
     }
 
+    // accessor method for color
     public String getColor() {
         return color;
     }
 
+    // accessor method for fuelType
     public String getFuelType() {
         return fuelType;
     }
 
+    // mutator method for engineSize
     public void setEngineSize(float engineSize) {
         this.engineSize = engineSize;
     }
 
+    // mutator method for numberOfWheels
     public void setNumberOfWheels(int numberOfWheels) {
        this.numberOfWheels = numberOfWheels;
     }
 
+    // mutator method for color
     public void setColor(String color) {
        this.color = color;
     }
 
+    //mutator method for fuelTypes
     public void setFuelType(String fuelType) {
        this.fuelType = fuelType;
     }
