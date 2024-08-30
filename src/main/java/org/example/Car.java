35943bd8310757bad45d57fee35d55401cde7ba3
Arwa Lokhandwala <arwajlokhandwala@gmail.com>
2024-08-29 21:35:19 -0400

    Car class created

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -2,18 +2,33 @@ package org.example;
 
 
 
-public class Car {
+public class Car extends Vehicle {
 
+    private String brand;
 
+    public Car(int numberOfWheels, String color, float engineSize, String fuelType, String brand){
+        super(numberOfWheels, color, engineSize, fuelType);
+        this.brand = brand;
+    }
 
+    public String getBrand() {
+        return brand;
+    }
 
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 
+    public void honk(){
+        System.out.println("Honk, honk!");
+    }
 
+    public String displayInfo(){
 
-
-
-
-
-
-
+        return "Car Brand: " + brand +
+                "/nColor: " + getColor() + "/nEngine Size: " + getEngineSize()
+                + "/nFuel Type: " + getFuelType() + "/Number of Wheels: " + getNumberOfWheels();
+    }
 }
+
+
